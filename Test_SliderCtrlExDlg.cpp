79b789d5d8c2697a0362003a069e04cd5a674b1c
scpark98 <scpark98@gmail.com>
2025-05-13 17:48:11 +0900

    .

--- Test_SliderCtrlExDlg.cpp
+++ Test_SliderCtrlExDlg.cpp
@@ -169,6 +169,7 @@ BOOL CTest_SliderCtrlExDlg::OnInitDialog()
 	m_slider_progress.SetPos(max / 2);
 	m_slider_progress.set_track_color(gRGB(36, 160, 212), gRGB(230, 230, 230));
 	m_slider_progress.set_text_style(CSCSliderCtrl::text_style_user_defined);
+	m_slider_progress.set_font_size(8);
 	m_slider_progress.draw_progress_border();
 	m_slider_progress.set_text(_T("alskdjf"));
 	m_slider_progress.set_use_slide();
@@ -344,7 +345,7 @@ LRESULT CTest_SliderCtrlExDlg::on_message_CSCSliderCtrl(WPARAM wParam, LPARAM lP
 	if (msg->pos > 80)
 		m_slider_progress.set_active_color(Gdiplus::Color::Red);
 	else
-		m_slider_progress.set_active_color(RGB(36, 160, 212));
+		m_slider_progress.set_active_color(gRGB(36, 160, 212));
 
 	m_slider_progress_dual_text.set_text(_T("%d ~ %d"), m_slider_progress_dual_text.get_lower(), msg->pos);
 	m_slider_progress_dual_text.set_text_dual(_T("%d ~ %d"), msg->pos, m_slider_progress_dual_text.get_upper());
